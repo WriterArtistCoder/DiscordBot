014a3b1b7a68c771c099e5f487998334b3b05b8f
WriterArtistCoder <41657434+WriterArtistCoder@users.noreply.github.com>
2019-09-27 10:02:58 -0700

    Cleaned out files from original

--- src/main/java/org/jointheleague/discord_bot_example/Bot.java
+++ src/main/java/org/jointheleague/discord_bot_example/Bot.java
@@ -2,15 +2,6 @@ package org.jointheleague.discord_bot_example;
 
 import org.javacord.api.DiscordApi; 
 import org.javacord.api.DiscordApiBuilder;
-import org.jointheleague.modules.CalculatorMessageListener;
-import org.jointheleague.modules.RandomNumber;
-import org.jointheleague.modules.Weather;
-import org.jointheleague.modules.DadJokes;
-import org.jointheleague.modules.ClockMessageListener;
-import org.jointheleague.modules.ComicMessageListener;
-import org.jointheleague.modules.ElmoMessageListener;
-import org.jointheleague.modules.FactMessageListener;
-import org.jointheleague.modules.FashionAdvisor;
 
 public class Bot  {
 
